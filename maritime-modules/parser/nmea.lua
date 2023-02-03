b4c14c785a742e51cedbb08fff454b9c0dc781a4
Florian Zidar PC <florian.zidar@arche.de>
2023-02-03 13:44:03 +0100

    replaced pgsub() with lua plaintext string.find(..)

--- maritime-modules/parser/nmea.lua
+++ maritime-modules/parser/nmea.lua
@@ -10,7 +10,7 @@ function parser_nmea:find_sentence(buffer)
     local msg = buffer():string()
     local pattern = "([!%$]%u%w-%p.-%*[%d%u][%d%u])\r\n"
     local sentence = string.match(buffer(0, -1):string(), pattern)
-    local sentence_beg, sentence_end = string.find(msg, utilities:pgsub(sentence))
+    local sentence_beg, sentence_end = string.find(msg, sentence, 1, true)
     return utilities:offset_length(sentence_beg, sentence_end)
 end
 
@@ -18,7 +18,7 @@ function parser_nmea:find_sentence_load(buffer)
     local msg = buffer():string()
     local pattern = "[!%$](%u%w-%p.-)%*[%d%u][%d%u]\r\n"
     local sentence = string.match(buffer(0, -1):string(), pattern)
-    local sentence_beg, sentence_end = string.find(msg, utilities:pgsub(sentence))
+    local sentence_beg, sentence_end = string.find(msg, sentence, 1, true)
     return utilities:offset_length(sentence_beg, sentence_end)
 end
 
@@ -27,7 +27,7 @@ function parser_nmea:find_talker_id(buffer)
     local pattern = "[!%$](%u%w-)%p.-%*[%d%u][%d%u]\r\n"
     local talker_sentence_ids = string.match(buffer(0, -1):string(), pattern)
     local talkerid = talker_sentence_ids:sub(1, -4)
-    local talk_id_beg, talk_id_end = string.find(msg, utilities:pgsub(talkerid))
+    local talk_id_beg, talk_id_end = string.find(msg, talkerid, 1, true)
     return utilities:offset_length(talk_id_beg, talk_id_end)
 end
 
@@ -36,7 +36,7 @@ function parser_nmea:find_sentence_id(buffer)
     local pattern = "[!%$](%u%w-)%p.-%*[%d%u][%d%u]\r\n"
     local talker_sentence_id = string.match(buffer(0, -1):string(), pattern)
     local sentenceid = talker_sentence_id:sub(-3, -1)
-    local sent_id_beg, sent_id_end = string.find(msg, utilities:pgsub(sentenceid))
+    local sent_id_beg, sent_id_end = string.find(msg, sentenceid, 1, true)
     return utilities:offset_length(sent_id_beg, sent_id_end)
 end
 
@@ -44,7 +44,7 @@ function parser_nmea:find_data(buffer)
     local msg = buffer():string()
     local pattern = "[!%$]%u%w-%p(.-)%*[%d%u][%d%u]\r\n"
     local data = string.match(buffer(0, -1):string(), pattern)
-    local data_beg, data_end = string.find(msg, utilities:pgsub(data))
+    local data_beg, data_end = string.find(msg, data, 1, true)
     return utilities:offset_length(data_beg, data_end)
 end
 
@@ -52,7 +52,7 @@ function parser_nmea:find_checksum(buffer)
     local msg = buffer():string()
     local pattern = "[!%$]%u%w-%p.-%*([%d%u][%d%u])\r\n"
     local chksm = string.match(buffer(0, -1):string(), pattern)
-    local chksm_beg, chksm_end = string.find(msg, utilities:pgsub(chksm))
+    local chksm_beg, chksm_end = string.find(msg, chksm, 1, true)
     return utilities:offset_length(chksm_beg, chksm_end)
 end
 
